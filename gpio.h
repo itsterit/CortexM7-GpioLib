21fff119db145bbcb4b213f715a138b9764a0851
itsterit <49567814+itsterit@users.noreply.github.com>
2023-04-27 09:57:22 +0400

    gpio lib add port settings

--- gpio.h
+++ gpio.h
@@ -38,35 +38,75 @@ extern "C"
  */
 #define DIGITALWRITE(GPIOx, GPIO_BSRR_Bxy) (GPIOx->BSRR |= GPIO_BSRR_Bxy);
 
+typedef enum
+{
+  eGPIOA = GPIOA,
+  eGPIOB = GPIOB,
+  eGPIOC = GPIOC,
+  eGPIOD = GPIOD,
+  eGPIOE = GPIOE
+} eGPIOx;
 
 typedef enum
 {
-  INPUT_MODE        = (0b00),
-  OUTPUT_MODE       = (0b01),
-  ALTERNATE_MODE    = (0b10),
-  ANALOG_MODE       = (0b11)
-} MODER;
+  eMODEx_Port_0  = GPIO_MODER_MODE0_Pos,
+  eMODEx_Port_1  = GPIO_MODER_MODE1_Pos,
+  eMODEx_Port_2  = GPIO_MODER_MODE2_Pos,
+  eMODEx_Port_3  = GPIO_MODER_MODE3_Pos,
+  eMODEx_Port_4  = GPIO_MODER_MODE4_Pos,
+  eMODEx_Port_5  = GPIO_MODER_MODE5_Pos,
+  eMODEx_Port_6  = GPIO_MODER_MODE6_Pos,
+  eMODEx_Port_7  = GPIO_MODER_MODE7_Pos,
+  eMODEx_Port_8  = GPIO_MODER_MODE8_Pos,
+  eMODEx_Port_9  = GPIO_MODER_MODE9_Pos,
+  eMODEx_Port_10 = GPIO_MODER_MODE10_Pos,
+  eMODEx_Port_11 = GPIO_MODER_MODE11_Pos,
+  eMODEx_Port_12 = GPIO_MODER_MODE12_Pos,
+  eMODEx_Port_13 = GPIO_MODER_MODE13_Pos,
+  eMODEx_Port_14 = GPIO_MODER_MODE14_Pos,
+  eMODEx_Port_15 = GPIO_MODER_MODE015_Pos
+} eMODEx_Port;
 
 typedef enum
 {
-  OUTPUT_PUSH_PULL  = (0b0),
-  OUTPUT_OPEN_DRAIN = (0b1)
-} OTYPER;
+  eMODE_INPUT_MODE        = (0b00),
+  eMODE_OUTPUT_MODE       = (0b01),
+  eMODE_ALTERNATE_MODE    = (0b10),
+  eMODE_ANALOG_MODE       = (0b11)
+} eMODER;
 
 typedef enum
 {
-  LOW_SPEED         = (0b00),
-  MEDIUM_SPEED      = (0b01),
-  HIGH_SPEED        = (0b10),
-  VERY_HIGH_SPEED   = (0b11)
-} OSPEEDR;
+  eOTYPE_OUTPUT_PUSH_PULL  = (0b0),
+  eOTYPE_OUTPUT_OPEN_DRAIN = (0b1)
+} eOTYPER;
 
 typedef enum
 {
-  NO_PULL           = (0b00),
-  PULL_UP           = (0b01),
-  PULL_DOWN         = (0b10)
-} PUPDR;
+  eOSPEED_LOW_SPEED         = (0b00),
+  eOSPEED_MEDIUM_SPEED      = (0b01),
+  eOSPEED_HIGH_SPEED        = (0b10),
+  eOSPEED_VERY_HIGH_SPEED   = (0b11)
+} eOSPEEDR;
+
+typedef enum
+{
+  ePUPD_NO_PULL           = (0b00),
+  ePUPD_PULL_UP           = (0b01),
+  ePUPD_PULL_DOWN         = (0b10)
+} ePUPDR;
+
+typedef struct
+{
+  eGPIOx GPIOx;
+
+  eMODEx_Pos MODEx_Port;
+  eMODER   MODER;
+
+  eOTYPER  OTYPER;
+  eOSPEEDR OSPEEDR;
+  ePUPDR   PUPDR;
+} GpioConfig;
 
 #endif /* GPIO_H */
 #ifdef __cplusplus
